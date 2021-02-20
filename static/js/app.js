657150acd5cad083f0808d10b80a9004e3647df1
yperez0914 <71104698+yperez0914@users.noreply.github.com>
2021-02-20 14:29:44 -0500

    demo_info

--- static/js/app.js
+++ static/js/app.js
@@ -1,30 +1,69 @@
 function buildMetadata(sample){
     d3.json("static/js/samples.json").then((data) => { 
         var metadata = data.metadata;
-        // var table = d3.select("#sample-metadata");
-        var filteredData = metadata.filter(i  => i.id === sample);
+        var table = d3.select("#sample-metadata");
+        table.html("")
+        var filteredData = metadata.filter(i  => i.id === parseInt (sample))[0];
         console.log(metadata);
-        console.log(filteredData);
+       
         
-        // Object.entries(filteredData).forEach(([key,value]) => {
-        //     table.append("div").text(`${key}: ${value}`);
-        //     console.log(filteredData);
+        Object.entries(filteredData).forEach(([key,value]) => {
+            table.append("div").text(`${key}: ${value}`);
         })
-        // })
+        console.log(filteredData);
+        })
        
     
 }
 
-// function buildCharts (sample){
-    // d3.json("static/js/samples.json").then((data) => { 
+function buildCharts (sample){
+    d3.json("static/js/samples.json").then((data) => {
     // Use sample_values as the values for the bar chart.
     // Use otu_ids as the labels for the bar chart.
     // Use otu_labels as the hovertext for the chart.
-    // var sample_values = data.samples.sample_values;
-    // var otu_ids = data.samples.otu_ids;
-    // var otu_labels = data.samples.otu_labels; 
-//     return 
-// }
+    var sample_values = data.samples.sample_values;
+    console.log(`${sample_values}`);
+    var sample_slice =sample_values.slice(0,10);
+    var otu_ids = data.samples.otu_ids;
+    var otu_slice = otu_ids.slice(0,10);
+    var otu_labels = data.samples.otu_labels; 
+    console.log(sample_slice);
+    console.log(sample_values);
+    // Chart 1
+    var layout = {
+        title: "Bacterial Samples"
+        };
+        // HINT: You will need to use slice() to grab the top 10 sample_values,
+        // otu_ids, and labels (10 each).
+        var bac = [{
+        // x: sample_values.slice(0, 11),
+        // y: otu_ids.slice(0, 11),
+        x: sample_slice,
+        y: otu_slice,
+        type: "bar",
+        orientation: 'h'
+        }];
+        Plotly.newPlot("bar", bac, layout, {responsive: true});
+    //Chart 2
+        var trace = [{
+        x: otu_slice,
+        y: sample_slice,
+        text: otu_labels,
+        mode: 'markers',
+        marker: {
+            size:sample_values,
+            color: otu_ids
+        }
+        }];
+        var layout2 = {
+        title: "Bacterial Bubble Chart",
+        showlegend: false
+        };
+        Plotly.newPlot("bubble", trace, layout2, {responsive: true});
+    })
+    }
+
+
 
 function init(){
     d3.json("static/js/samples.json").then((data) => {
@@ -39,7 +78,7 @@ function init(){
             });
             var first_sample= id_names[0];
             buildMetadata(first_sample);
-            // buildCharts(first_sample);
+            buildCharts(first_sample);
 
 
 
@@ -50,45 +89,10 @@ function init(){
 
 
 function optionChanged(sample) {
-    // Prevent the page from refreshing
-    // d3.event.preventDefault();
-    // d3.json("static/js/samples.json").then((data) => { 
-    // Select the dropdown
+    
     var dropdown = d3.select("#onChange");
-    // var inputValue = dropdown.property("value");
     console.log(sample);
-    // Create event handlers 
-    dropdown.on("change", buildMetadata(sample));
-    // .on("change", buildCharts(value));
-    // })
+    dropdown.on("change", buildMetadata(sample)).on("change", buildCharts(value));
 }
-
-// // Complete the event handler function for the form
-// function runEnter() {
-
-//   // Prevent the page from refreshing
-//   d3.event.preventDefault();
-  
-//   // Select the input element and get the raw HTML node
-//   var inputElement = d3.select("#patient-form-input");
-
-//   // Get the value property of the input element
-//   var inputValue = inputElement.property("value");
-
-//   console.log(inputValue);
-//     console.log(sample_values);
-//     })
-// }
-
-
-// var trace1 = [{
-//     type: 'bar',
-//     x: 
-//     y: 
-//     text: 
-//     orientation: 'h'
-//   }];
-  
-//   Plotly.newPlot('#bar', data);
 
 init();
